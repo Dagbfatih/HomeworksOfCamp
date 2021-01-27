2e79f87c27fa230bbcc1ccf8779ae2dc94a92af0
Dagbfatih <Dagbfatih@users.noreply.github.com>
2021-01-27 11:42:47 +0300

    Customer Follower

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+
+        public void AddCustomer(ref List<Customer> customers, ref int IDCounter)
+        {
+
+            Customer customerCreate = new Customer();
+
+            Console.WriteLine("Name: ");                    // Name
+            customerCreate.Name = Console.ReadLine();       
+            Console.WriteLine("\nSurname: ");               // Surname
+            customerCreate.Surname = Console.ReadLine();
+            Console.WriteLine("\nAge: ");                   // Age
+            customerCreate.Age = Convert.ToInt32(Console.ReadLine());
+            customerCreate.ID = IDCreator(ref IDCounter);   // ID
+
+            customers.Add(customerCreate);
+            Console.WriteLine("İtem Added...");
+        }
+
+        public void DeleteCustomer(ref List<Customer> customers, ref int IDCounter)
+        {
+            IDCounter--;
+            customers.RemoveAt(customers.Count - 1);
+            Console.WriteLine("İtem Deleted...");
+        }
+
+        public void ListCustomers(ref List<Customer> customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Name :\t\t" + customer.Name);
+                Console.WriteLine("Surname :\t" + customer.Surname);
+                Console.WriteLine("Age :\t\t" + customer.Age);
+                Console.WriteLine("ID :\t\t" + customer.ID);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private string IDCreator(ref int IDCounter)
+        {
+            IDCounter++;
+            string ID = (IDCounter < 10 ? "00" : IDCounter < 100 ? "0" : "") + Convert.ToString(IDCounter);
+            return ID;
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IDCounter = 1;
+
+            Customer customer1 = new Customer();
+            customer1.Name = "Fatih";
+            customer1.Surname = "Dag";
+            customer1.Age = 16;
+            customer1.ID = "000"; // 001 - 002 ... 009 - 010 - 011
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Ahmet";
+            customer2.Surname = "Yilmaz";
+            customer2.Age = 56;
+            customer2.ID = "001";
+
+            List<Customer> customers = new List<Customer>() { customer1, customer2 }; //Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+            CustomerManager customerManager = new CustomerManager();
+
+            while (true)
+            {
+                
+                Console.WriteLine("For Add Customer Press 1, For Delete Customer Press 2, For List Customers Press 3, For Exit Press 4");
+                var key = Console.ReadKey().KeyChar;
+                Console.Clear();
+
+                if (key == '1')
+                {
+                    customerManager.AddCustomer(ref customers, ref IDCounter);
+                }
+                else if (key == '2')
+                {
+                    customerManager.DeleteCustomer(ref customers, ref IDCounter);
+                }
+                else if (key == '3')
+                {
+                    customerManager.ListCustomers(ref customers);
+                }
+                else if (key == '4')
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Re-press...");
+                }
+            }
+            
+        }
+    }
+}
