9f11df7b68f8e6acfbf2fda0cf46aca3fac8f2be
Dagbfatih <Dagbfatih@users.noreply.github.com>
2021-01-29 18:12:54 +0300

    my dictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+
+        TKey[] _keyArray, _tempKeyArray;
+        TValue[] _valueArray, _tempValueArray;
+
+
+        public MyDictionary()
+        {
+            _keyArray = new TKey[0];
+            _valueArray = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _tempKeyArray = _keyArray;
+            _tempValueArray = _valueArray;
+
+            _keyArray = new TKey[_keyArray.Length + 1];
+            _valueArray = new TValue[_valueArray.Length + 1];
+
+            for (int i = 0; i < _tempKeyArray.Length; i++)
+                _tempKeyArray[i] = _keyArray[i];
+
+            for (int i = 0; i < _tempValueArray.Length; i++)
+                _tempValueArray[i] = _valueArray[i];
+
+            _keyArray[_keyArray.Length - 1] = key;
+            _valueArray[_valueArray.Length - 1] = value;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> dictionary1 = new Dictionary<int, int>();
+            dictionary1.Add(1, 3);
+
+            MyDictionary<int, string> dictionary2 = new MyDictionary<int, string>();
+            dictionary2.Add(2, "Adıyaman");
+        }
+    }
+}
